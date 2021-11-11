cdcf05fed3a373a3560a0e90a8f858649c4970ce
lilidog <lilin@lilin.site>
2021-11-11 12:44:19 +0800

    增加读块

--- examples/testCardReader.js
+++ examples/testCardReader.js
@@ -15,27 +15,29 @@ const { CardReader: CR } = require("../src/index");
 }();
 
 async function main () {
-    let index = 0;
+    // let index = 0;
     const cr = new CR({ path: "COM8", baudRate: 9600 });
-    cr.on("packet", packet => {
-        console.log("================================> " + (index++));
-        console.log(packet);
-        console.log(Array.from(packet.data).reduce((t, c) => {
-            t += c.toString(16);
-            return t;
-        }));
-        cr.beep(2, 50, 20);
-    });
-    await cr.init();
-    console.log(await cr.status());
-    await cr.setStatus(new CR.Status({ 
-        work_mode: 2,
-        auto_read_block: 0,
-        upload_mode: 0, 
-        beep_enable: 0,
-        auto_read_mode: 0
-    }));
-    console.log(await cr.status());
+    // cr.on("packet", packet => {
+    //     console.log("================================> " + (index++));
+    //     console.log(packet);
+    //     console.log(Array.from(packet.data).reduce((t, c) => {
+    //         t += c.toString(16);
+    //         return t;
+    //     }));
+    //     cr.beep(2, 50, 20);
+    // });
+    // await cr.init();
+    // console.log(await cr.status());
+    // await cr.setStatus(new CR.Status({ 
+    //     work_mode: 2,
+    //     auto_read_block: 0,
+    //     upload_mode: 0, 
+    //     beep_enable: 0,
+    //     auto_read_mode: 0
+    // }));
+    // console.log(await cr.status());
     // await cr.beep(1);
-    console.log("end");     
+    // console.log("end");  
+    
+    console.log(cr.readBlock(4, "a", true));
 }
